f57d26ec02d1e27fc7bcceb267d4598f77b5c092
Lovro <lovro.gamulin@gmail.com>
2022-10-17 16:45:40 +0200

    ignoriranje exceptiona

---
+++ Recepati/Code/Managers/Migrator.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using DbUp;
+using Recepati.Db.Code;
+using System.Reflection;
+
+namespace Recepati.Code.Models
+{
+    public class Migrator
+    {
+        private PublicConn _conn { get; set; }
+        public void RunUpgrade()
+        {
+            var connectionString = _conn.ConnectionString;
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), f => f.Contains("Migrator"))
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            var test = 1;
+        }
+
+        public Migrator(PublicConn conn)
+        {
+            _conn = conn;
+        }
+
+    }
+}
